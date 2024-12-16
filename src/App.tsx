1dcfceed3143bef7056ec6360535d22dabfa7b63
olddevai <pompom.kah@gmail.com>
2024-12-16 14:56:47 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -102,7 +102,7 @@ function App() {
 
       {/* Floating Telegram Button */}
       <a
-        href="https://t.me/yourTelegramChannel"
+        href="https://t.me/nullsrc"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-4 right-4 bg-blue-500 text-white rounded-full p-4 shadow-lg hover:bg-blue-600 transition-colors"
